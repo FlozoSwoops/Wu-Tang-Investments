12d1b65ee4cf689933b291a7a69a78f260374dc8
flozoswoops <flores.a.mark@gmail.com>
2017-11-11 20:19:57 -0500

    show route working

--- app/controllers/api/investors_controller.rb
+++ app/controllers/api/investors_controller.rb
@@ -1,2 +1,13 @@
 class Api::InvestorsController < ApplicationController
+    def index
+        @investors = Investor.all
+        render json: @investors
+    end
+
+    def show
+        @investor = Investor.find(params[:id])
+        render json: @investor
+    end
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :investors do
-      resources :portfolios do
-        resources :stocks
-      end
     end
   end
 end
