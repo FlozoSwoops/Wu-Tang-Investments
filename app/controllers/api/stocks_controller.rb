e1ef5b7146eeb78072d4add5ef3290abeb922595
flozoswoops <flores.a.mark@gmail.com>
2017-11-11 23:40:01 -0500

    stock create route working

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -11,7 +11,18 @@ class Api::StocksController < ApplicationController
     end
 
     def create
-
+        @investor = Investor.find(params[:investor_id])
+        @portfolio = Portfolio.find(params[:portfolio_id])
+        @stock = Stock.new(stock_params)
+        @portfolio.stocks << @stock
+        @stock.save!
+        @stocks = @portfolio.stocks
+        render json: @stocks
     end
 
+    private
+    
+    def stock_params
+        params.require(:stock).permit(:symbol, :lastprice, :shares)
+    end
 end
