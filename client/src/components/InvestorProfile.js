1981fb63a2c1699a46947e6d2db43cf6188f9b7d
flozoswoops <flores.a.mark@gmail.com>
2017-11-13 15:16:37 -0500

    can navigate to portfolio show page

--- client/src/components/InvestorProfile.js
+++ client/src/components/InvestorProfile.js
@@ -1,35 +1,48 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 class InvestorProfile extends Component {
 
     state = {
         investor: {},
-        portfolio: []
+        portfolios: []
     }
     async componentWillMount() {
-        try { 
+        try {
             const investorId = this.props.match.params.investorId
             const response = await axios.get(`/api/investors/${investorId}`)
             console.log(response)
-            this.setState({ investor: response.data})
-        }catch (error) {
+            this.setState({ investor: response.data })
+        } catch (error) {
             console.log(error)
         }
         try {
             const investorId = this.props.match.params.investorId
             const response = await axios.get(`/api/investors/${investorId}/portfolios`)
-            this.setState({ portfolio: response.data})
-        } catch(error) {
+            this.setState({ portfolios: response.data })
+        } catch (error) {
             console.log(error)
         }
     }
     render() {
         return (
+
+
+
             <div>
-            <h1>{this.state.investor.name}'s Portfolios</h1>
-            {/* add cards w/ a pie chart of portfolio diversity */}
+                <h1>{this.state.investor.name}'s Portfolios</h1>
+                <ul>
+                    {this.state.portfolios.map((portfolio) => {
+                        return (
+                            <Link to={`/investors/${this.state.investor.igitd}/portfolios/${portfolio.id}`}>{portfolio.risk}</Link>
+
+                        )
+                    })}
+                    {/* add cards w/ a pie chart of portfolio diversity */}
+                </ul>
             </div>
+
 
 
         );
