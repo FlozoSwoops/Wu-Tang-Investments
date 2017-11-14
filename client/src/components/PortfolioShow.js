5427788352c8b04aae3aedcefb52f700151e2d5f
flozoswoops <flores.a.mark@gmail.com>
2017-11-14 10:18:09 -0500

    Changed migration file back to CreateNames

--- client/src/components/PortfolioShow.js
+++ client/src/components/PortfolioShow.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 class PortfolioShow extends Component {
+    state = {
+        portfolio: {},
+        stocks:[]
+    }
+
+    async componentWillMount() {
+        try {
+            const investorId = this.props.match.params.investorId
+            const response = await axios.get(`/api/investors/${investorId}/portfolios`)
+            this.setState({ portfolios: response.data })
+        } catch (error) {
+            console.log(error)
+        }
+        try {
+            const investorId = this.props.match.params.investorId
+            const portfolioId = this.props.match.params.portfolioId
+            const response = await axios.get(`/api/investors/${investorId}/portfolios/${portfolioId}/stocks`)
+            this.setState({ stocks: response.data})
+            } catch(error) {
+                console.log(error)}
+            
+            }
+    
     render() {
         return (
             <div>
+                Portfolio information
+
                 
             </div>
         );
