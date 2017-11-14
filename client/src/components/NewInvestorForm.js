d593724c1c1274140e221e101a2206e4a15ac019
flozoswoops <flores.a.mark@gmail.com>
2017-11-14 12:25:46 -0500

    creating form for investor

---
+++ client/src/components/NewInvestorForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class NewInvestorForm extends Component {
+    
+    state = {
+        newInvestor: {
+            name: '',
+            username: '',
+            occupation: '',
+            income: ''
+        },
+    }
+
+
+    
+    
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default NewInvestorForm;
--- client/src/components/PortfolioShow.js
+++ client/src/components/PortfolioShow.js
@@ -5,14 +5,14 @@ import axios from 'axios'
 class PortfolioShow extends Component {
     state = {
         portfolio: {},
-        stocks:[]
+        stocks: []
     }
 
     async componentWillMount() {
         try {
             const investorId = this.props.match.params.investorId
             const response = await axios.get(`/api/investors/${investorId}/portfolios`)
-            this.setState({ portfolios: response.data })
+            this.setState({ portfolio: response.data })
         } catch (error) {
             console.log(error)
         }
@@ -20,18 +20,34 @@ class PortfolioShow extends Component {
             const investorId = this.props.match.params.investorId
             const portfolioId = this.props.match.params.portfolioId
             const response = await axios.get(`/api/investors/${investorId}/portfolios/${portfolioId}/stocks`)
-            this.setState({ stocks: response.data})
-            } catch(error) {
-                console.log(error)}
-            
-            }
-    
+            this.setState({ stocks: response.data })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     render() {
         return (
             <div>
-                Portfolio information
-
+                <h2>Portfolio information</h2>
+                   <div><span><em>{this.state.portfolio.name}</em>  <em>{this.state.portfolio.value}</em></span></div>
+                   <div><span>{this.state.portfolio.risk}</span></div>
                 
+                <h3>Stocks</h3>
+
+                <div>
+                    {this.state.stocks.map((stock) => {
+                        return(
+                            <ul>
+                                <li><h4>{stock.symbol} {stock.lastprice} {stock.shares}</h4></li>
+                            </ul>
+                        )
+                    })}
+                </div>
+                
+
+
+
             </div>
         );
     }
